b0ae98f0a8efdf0ed78ca1aa45e3f32bc2ea452b
Brian Savage <savage@uri.edu>
2019-07-27 18:24:25 -0400

    Turn off dependency tracking

--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
-autoreconf --verbose  --install 
+# inc/config.h.in needs to be "younger" than configure.in
+#   Otherwise autoheader will attempted to be run, which we do not want
+#   autotools considers config.h.in to be a derivative of configure.in
+aclocal && \
+automake --add-missing --foreign --copy -i && \
+# autoheader && \
+touch config.h.in && \
+autoconf
 
-rm -rf autom4te.cache
+rm -rf autom4te.cache/
+
+#autoreconf --verbose  --install
+#rm -rf autom4te.cache
 
 
