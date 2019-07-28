2f10b4d9ee2a694de9b53befb20b849dca5b5772
Brian Savage <savage@uri.edu>
2019-07-28 07:11:39 -0400

    Use a normal build process

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ev
-# touch configure.ac aclocal.m4 configure Makefile.am Makefile.in
-# ls -lrt
-./configure && make && make check
--- config/bootstrap.sh
+++ config/bootstrap.sh
@@ -1,17 +1,6 @@
 #!/bin/sh
 
-# inc/config.h.in needs to be "younger" than configure.in
-#   Otherwise autoheader will attempted to be run, which we do not want
-#   autotools considers config.h.in to be a derivative of configure.in
-aclocal && \
-automake --add-missing --foreign --copy -i && \
-# autoheader && \
-touch config.h.in && \
-autoconf
-
-rm -rf autom4te.cache/
-
-#autoreconf --verbose  --install
-#rm -rf autom4te.cache
+autoreconf --verbose  --install
+rm -rf autom4te.cache
 
 
