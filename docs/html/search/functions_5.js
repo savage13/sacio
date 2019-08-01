2e4e3c2583771dabd3c8fe90ed4fe398e3793820
Brian Savage <savage@uri.edu>
2019-08-01 06:38:54 -0400

    [skip ci] Add timespec function to docs

--- docs/html/search/functions_5.js
+++ docs/html/search/functions_5.js
@@ -3,6 +3,7 @@ var searchData=
   ['timespec64_5fadd_5fduration',['timespec64_add_duration',['../group__time.html#ga45e3937f7d53d95f66c721ab807b2d2b',1,'timespec64']]],
   ['timespec64_5fcmp',['timespec64_cmp',['../group__time.html#ga5af92a170d9fd0d2a3c4858ee4e8be08',1,'timespec64']]],
   ['timespec64_5ffrom_5fyjhmsf',['timespec64_from_yjhmsf',['../group__time.html#gad177be50dbba74a92c7c64e390aaf3a0',1,'timespec64']]],
+  ['timespec64_5ffrom_5fymdhmsf',['timespec64_from_ymdhmsf',['../group__time.html#ga9ab66d38e97f85c3cc20002b488625e8',1,'timespec64']]],
   ['timespec64_5fnow',['timespec64_now',['../group__time.html#gab0464b8ec5341b8f6bb1e6e83e90fef3',1,'timespec64']]],
   ['timespec64_5fparse',['timespec64_parse',['../group__time.html#ga5785dce1a99b1cb77784303751fa1900',1,'timespec64']]],
   ['timespec64_5fprint',['timespec64_print',['../group__time.html#gad73ea8cd49c01cb462dd46bea6ebe2ac',1,'timespec64']]]
