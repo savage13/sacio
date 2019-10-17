ba812c1f5d26f962fa1c82e960b674d7d77e5193
Brian Savage <savage@uri.edu>
2019-10-17 19:30:50 -0400

    [no ci] Update documentation

---
+++ docs/html/search/classes_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['timespec64',['timespec64',['../structtimespec64.html',1,'']]]
+];
--- docs/html/search/functions_7.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['wsac0',['wsac0',['../group__sac-iris.html#ga217d782d6d9b07c99ca631c326079170',1,'sac_iris']]],
-  ['wsac1',['wsac1',['../group__sac-iris.html#ga2c90b56e40f85cc9bb98fb0898c4805f',1,'sac_iris']]],
-  ['wsac2',['wsac2',['../group__sac-iris.html#ga91da0d15bab46b5f512976fb23dd1b30',1,'sac_iris']]]
-];
